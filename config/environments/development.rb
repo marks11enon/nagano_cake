cb600044ca7fde40da3446d52166b2b7179c5cd7
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-02-21 15:10:18 +0000

    [Add]顧客homes items controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "0e873ac58c444b11b67eecd0acdb31a0.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
+
 end
