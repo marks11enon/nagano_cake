cb7eb143751676dfd42efa14603baeaa48377ae3
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-02-27 07:02:34 +0000

    [Add] item new view

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  has_many :cart_items, dependment: :destroy
+  has_one_attached :image
+  validates :is_active, inculusion: { in: [true, false] }
 end
