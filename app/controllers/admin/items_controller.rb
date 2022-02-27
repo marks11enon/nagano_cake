336c4e6c9c61020f98e745a4ee57ee5832fcbe04
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-02-27 08:21:27 +0000

    [Update] item new view -collection_select

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,4 +17,9 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :is_active)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :genre
-  has_many :cart_items, dependment: :destroy
+  has_many :cart_items, dependent: :destroy
   has_one_attached :image
-  validates :is_active, inculusion: { in: [true, false] }
+  validates :is_active, inclusion: { in: [true, false] }
 end
