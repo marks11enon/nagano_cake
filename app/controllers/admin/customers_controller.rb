14b6aaf7bb8b3dbef888d7d689bdc6767ceef789
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-03-03 13:38:48 +0000

    [Update] customer index view

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,4 +13,9 @@ class Admin::CustomersController < ApplicationController
 
   def update
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,8 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders
   has_many :addresses
+  # 会員フルネーム
+  def full_name
+    self.last_name + " " + self.first_name
+  end
 end
