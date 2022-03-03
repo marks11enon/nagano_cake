2f10081715fdb3b0a036edca3a71a8ffd0d34831
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-03-03 15:51:16 +0000

    [Add] customer index show view

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,14 +1,16 @@
 class Admin::CustomersController < ApplicationController
   # このcontrollerを記述する　3/3 private paramsが必要　indexも確認
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
     @customer = Customer.new
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
@@ -16,6 +18,7 @@ class Admin::CustomersController < ApplicationController
 
   private
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana,
+    :first_name_kana, :postal_code, :address, :telephone_number)
   end
 end
