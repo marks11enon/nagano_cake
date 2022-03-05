53bca6616e7d8603916d3a1a93d740f00814037e
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-03-05 16:39:59 +0000

    [Update] routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
     root to: "homes#top"
@@ -28,7 +16,8 @@ Rails.application.routes.draw do
     get "/customers/my_page" => "customers#show", as: "customers_my_page"
     get "/customers/unsubscribe" => "customers#unsubscribe", as: "unsubscribe"
     patch "/customers/withdraw" => "customers#withdraw", as: "withdraw"
-    resources :customers, only: [:edit, :update]
+    get "/customers/edit" => "customers#edit", as: "edit"
+    resources :customers, only: [:update]
     delete "/cart_items/destroy_all" => "cart_items#destroy_all", as: "destroy_all"
     resources :cart_items, only: [:index, :update, :destroy, :create]
     get "/orders/comfirm" => "orders#comfirm", as: "comfirm"
@@ -36,5 +25,17 @@ Rails.application.routes.draw do
     resources :orders, only: [:new, :create, :index, :show]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
   end
+   # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
 
 end
