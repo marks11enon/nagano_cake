c47959e256177b90a5c3e34e066bfbfef950f488
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-03-05 16:17:14 +0000

    [Add] public customer show view

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,16 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page.current_customer
   end
 
   def unsubscribe
