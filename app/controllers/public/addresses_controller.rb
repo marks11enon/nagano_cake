cc86b5a0cb30db6e1a0d434bb2ea8cac44d8c3b1
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-03-06 14:42:01 +0000

    [Add] address index view

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,48 @@
 class Public::AddressesController < ApplicationController
   def index
-  end
-
-  def edit
+    @customer = current_customer
+    @addresses = @customer.addresses
+    @address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash[:success] = "登録に成功しました"
+      redirect_to addresses_path
+    else
+      @customer = current_customer
+      @address = @customer.address.all
+      flash[:warning] = "入力内容を確認してください"
+      render :index
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    address = Address.find(params[:id])
+    if address.update(address_params)
+      redirect_to addresses_path
+      flash[:success] = "更新に成功しました"
+    else
+      flash[:success] = "入力内容を確認してください"
+      render :edit
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    flash[:success] = "削除に成功しました"
+    redirect_to addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 end
