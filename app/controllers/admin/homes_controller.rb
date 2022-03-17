fac96f32500a804716396e7a27f98aff1cbf5c9f
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-03-17 15:25:32 +0000

    [Add-Finish] customers edit [Add]order_details

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,11 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.page(params[:page]).per(10)
+
   end
+
+  private
+	def order_params
+		params.require(:order).permit(:status, )
+	end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,21 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def update
+    order = Order.find(params[:id])
+    order_details = order.order_details
+    order.update(order_params)
+    if order.status == "入金確認"
+      order_details.update_all(maiking_status: "製作待ち")
+    end
+    redirect_to admin_order_path(:status)
+  end
+
+  private
+  def order_params
+    params.requier(:order).permit(:status)
   end
 end
