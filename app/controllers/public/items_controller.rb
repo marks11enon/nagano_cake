08d1db10355dc16c616d8b23208cbddacf01e2d1
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-03-19 05:08:24 +0000

    [Update] layout

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
     if params["genre"]
-      @items = Item.active.where(genre_id: params["genre"])
+      @items = Item.active.where(genre_id: params["genre"]).page(params[:page]).per(8)
     else
-      @items = Item.active
+      @items = Item.active.page(params[:page]).per(8)
     end
     @genres = Genre.all
     @items_all = Item.all
+    @quantity = Item.count
   end
 
   def show
